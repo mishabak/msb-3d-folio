5a21f7cf06f2fc980cfd07fd4f1d78ce31fe4ade
mishab <mishab.ak@pardypanda.com>
2024-10-27 00:31:05 +0530

    add animation for touch pad

--- src/components/3D/ContactRoom/HologramTouchPad.jsx
+++ src/components/3D/ContactRoom/HologramTouchPad.jsx
@@ -1,17 +1,50 @@
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
 import { useDebounce } from "use-debounce";
-import { Fragment, useEffect, useState } from "react";
-
+import { Fragment, useEffect, useRef, useState } from "react";
 export function HologramTouchPad(props) {
-  const { nodes, materials } = useGLTF("/models/hologramTouchPad.glb");
-
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF(
+    "/models/hologramTouchPad.glb"
+  );
+  const { actions } = useAnimations(animations, group);
   const Collision = () => {
-    const [isEnter, setIsEnter] = useState(false);
-    const IsEnterStable = useDebounce(isEnter, isEnter ? 1000 : 200);
+    const [isEnter, setIsEnter] = useState(null);
+    const IsEnterStable = useDebounce(isEnter, 1917);
+
+    const handleAnimation = async (keyName) => {
+      if (actions[keyName]) {
+        actions[keyName].repetitions = 1;
+        actions[keyName].clampWhenFinished = true;
+        if (IsEnterStable[0]) {
+          actions[keyName].timeScale = 1;
+          actions[keyName]?.play();
+        } else if (!IsEnterStable[0]) {
+          actions[keyName].timeScale = -1;
+          actions[keyName]?.play();
+        }
+      }
+    };
 
     useEffect(() => {
-      console.log(IsEnterStable[0], "😀");
+      
+      [
+        "FooterAction",
+        "KeyboardAction",
+        "KeyboardFrameAction",
+        "LeftLeafAction",
+        "RightLeafAction",
+        "StandAction",
+      ].forEach((keyName) => handleAnimation(keyName));
+
+      return () => {
+        actions.FooterAction.stop();
+        actions.KeyboardAction.stop();
+        actions.KeyboardFrameAction.stop();
+        actions.LeftLeafAction.stop();
+        actions.RightLeafAction.stop();
+        actions.StandAction.stop();
+      };
     }, [IsEnterStable[0]]);
 
     return (
@@ -25,16 +58,18 @@ export function HologramTouchPad(props) {
         type="fixed"
         colliders={"cuboid"}
       >
-        <group position={[1.608, 2.241, 0.463]} scale={[0.381, 0.003, 0.381]}>
+        <group
+          name="floor"
+          position={[0.6, 0.005, 0]}
+          scale={[0.381, 0.003, 0.381]}
+        >
           <mesh
-            castShadow
-            receiveShadow
+            name="Cube"
             geometry={nodes.Cube.geometry}
             material={materials["Material.002"]}
           />
           <mesh
-            castShadow
-            receiveShadow
+            name="Cube_1"
             geometry={nodes.Cube_1.geometry}
             material={materials.Material}
           />
@@ -46,62 +81,56 @@ export function HologramTouchPad(props) {
   return (
     <Fragment>
       <group
+        ref={group}
         {...props}
         scale={1}
-        position={[0.4, -2.75, 0]}
+        position={[0, -0.52, 1.5]}
         rotation={[0, -Math.PI / 2, 0]}
         dispose={null}
       >
         <mesh
-          castShadow
-          receiveShadow
+          name="KeyboardFrame"
           geometry={nodes.KeyboardFrame.geometry}
           material={materials["Material.002"]}
-          position={[0.972, 3.419, 0.463]}
-          rotation={[0, 0, -0.717]}
-          scale={[0.261, 0.008, 0.352]}
+          position={[0.064, 1.282, 0]}
+          scale={0}
         />
         <mesh
-          castShadow
-          receiveShadow
+          name="LeftLeaf"
           geometry={nodes.LeftLeaf.geometry}
           material={materials["Material.002"]}
-          position={[0.971, 2.947, 0.458]}
-          scale={[0.129, 0.666, 0.096]}
+          position={[-0.037, 0.711, 0.795]}
+          scale={0}
         />
         <mesh
-          castShadow
-          receiveShadow
+          name="Footer"
           geometry={nodes.Footer.geometry}
           material={materials["Material.002"]}
-          position={[1.008, 2.259, 0.463]}
-          scale={[0.218, 0.021, 0.226]}
+          position={[0, -0.031, 0]}
+          scale={0}
         />
         <mesh
-          castShadow
-          receiveShadow
+          name="Keyboard"
           geometry={nodes.Keyboard.geometry}
           material={materials["Material.001"]}
-          position={[1.014, 3.461, 0.462]}
-          rotation={[0, 0, -0.708]}
-          scale={[0.192, 0.016, 0.268]}
+          position={[-0.028, 1.221, -0.001]}
+          scale={0}
         />
         <mesh
-          castShadow
-          receiveShadow
+          name="RightLeaf"
           geometry={nodes.RightLeaf.geometry}
           material={materials["Material.002"]}
-          position={[0.971, 2.947, 0.48]}
-          scale={[0.129, 0.666, 0.096]}
+          position={[-0.037, 0.711, -0.783]}
+          scale={0}
         />
         <mesh
-          castShadow
-          receiveShadow
+          name="Stand"
           geometry={nodes.Stand.geometry}
           material={materials["Material.002"]}
-          position={[0.971, 2.862, 0.463]}
-          scale={[0.053, 0.586, 0.053]}
+          position={[-0.038, -0.041, 0.002]}
+          scale={0}
         />
+
         <Collision />
       </group>
     </Fragment>
