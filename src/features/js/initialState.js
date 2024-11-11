3dc8d75ec436de0e1f807808d3db3f827d290fef
mishab <mishab.ak@pardypanda.com>
2024-11-11 12:05:47 +0530

    make first pulle false

--- src/features/js/initialState.js
+++ src/features/js/initialState.js
@@ -1,7 +1,7 @@
 export default {
   currentRoom: null,
   // initial state for puzzle  -->>
-  solvedPuzzle_1: true,
+  solvedPuzzle_1: false,
   solvedPuzzle_2: true,
   solvedPuzzle_3: true,
   solvedPuzzle_4: true,
