14b8d1b58fe25fa2bd076e3474faf9acb10cabba
mishab <mishab.ak@pardypanda.com>
2024-11-11 14:25:34 +0530

    add puzzle preview image

--- src/components/3D/Puzzle/PuzzleFrame.jsx
+++ src/components/3D/Puzzle/PuzzleFrame.jsx
@@ -1,17 +1,25 @@
 function PuzzleFrame({ nodes, materials }) {
   return (
-    <mesh
-      geometry={nodes.PuzzileFrame.geometry}
-      material={materials.PuzzileFrame}
-      position={[0.159, 0.31, -0.019]}
-      scale={[0.25, 0.25, 0.019]}
-    >
-      <meshStandardMaterial
-        metalness={1.4}
-        roughness={0.1}
-        envMapIntensity={1.5}
+    <group position={[0.159, 0.31, -0.019]} scale={[0.25, 0.25, 0.019]}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube001.geometry}
+        material={materials.PuzzileFrame}
       />
-    </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube001_1.geometry}
+        material={materials.Material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube001_2.geometry}
+        material={materials.Puzzle_image_1}
+      />
+    </group>
   );
 }
 
