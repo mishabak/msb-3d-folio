a1cfdc08bca4584a9863aa44ea6605755d6e365b
mishab <mishab.ak@pardypanda.com>
2024-11-14 12:18:24 +0530

    fix audio re initialize issue

--- src/components/3D/ContactRoom/Audio.jsx
+++ src/components/3D/ContactRoom/Audio.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from "react";
+import { useEffect } from "react";
 import useAudio from "../../../hooks/useAudio";
 import { useSelector } from "react-redux";
 import { selector_rooms } from "../../../features/js/selector";
@@ -7,27 +7,31 @@ function Audio() {
   const inputSelectAudio = useAudio({
     url: "/audio/hologram/selectEffect.mp3",
   });
-  const contactRoomSounds = useSelector(selector_rooms.contactRoomSounds);
 
-  const handleSound = async () => {
-    switch (contactRoomSounds.type) {
-      case "INPUT":
-      case "TEXT_AREA":
-        await inputSelectAudio.initialize();
-        contactRoomSounds.play
-          ? inputSelectAudio.audio.play()
-          : inputSelectAudio.audio.stop();
-        break;
-      default:
-        break;
-    }
+  const ChildHandler = () => {
+    const contactRoomSounds = useSelector(selector_rooms.contactRoomSounds);
+
+    const handleSound = async () => {
+      switch (contactRoomSounds.type) {
+        case "INPUT":
+        case "TEXT_AREA":
+          await inputSelectAudio.initialize();
+          contactRoomSounds.play
+            ? inputSelectAudio.audio.stop() && inputSelectAudio.audio.play()
+            : inputSelectAudio.audio.stop();
+          break;
+        default:
+          break;
+      }
+    };
+
+    useEffect(() => {
+      handleSound();
+    }, [contactRoomSounds.play, contactRoomSounds.type]);
+
+    return null;
   };
-
-  useEffect(() => {
-    handleSound();
-  }, [contactRoomSounds.play, contactRoomSounds.type]);
-
-  return null;
+  return <ChildHandler />;
 }
 
 export default Audio;
